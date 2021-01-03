e907a478b63557b9445fdb510827c44233661b5c
Purbayan Chowdhury <pur.cho.99@gmail.com>
2021-01-04 03:09:41 +0530

    change

--- ipynb.py
+++ ipynb.py
@@ -3,5 +3,6 @@ import os
 
 if __name__ == "__main__":
     files = glob.glob('./**/*.ipynb', recursive=True)
+    print(files)
     for file in files:
-        os.system(f"jupyter nbconvert –to html '{file}'")
+        os.system(f"jupyter nbconvert '{file}'")
