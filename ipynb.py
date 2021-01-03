6cc6ecc9e09e511cd0c19203f39c522f012b527c
Purbayan Chowdhury <pur.cho.99@gmail.com>
2021-01-04 02:10:11 +0530

    change ipynb.py

--- ipynb.py
+++ ipynb.py
@@ -1,4 +1,20 @@
-import glob, os
+import glob
+import os
 
-os.system('jupyter nbconvert –to html_embed \'{}\''.format("".join(glob.glob('./**/*.ipynb', recursive = True))))
+ghtoken = os.getenv('TOKEN')
 
+if __name__ == "__main__":
+
+    os.system('jupyter nbconvert –to html_embed \'{}\''.format(
+        "".join(glob.glob('./**/*.ipynb', recursive=True))))
+
+    try:
+        if ghtoken is None:
+            raise Exception('Token not available')
+        os.system('git add .')
+        os.system('git commit -m "Convert to HTML files"')
+        os.system(
+            f'git push https://Julia-Kolkata-User-Group:{ghtoken}@github.com/ExploreML-Julia HEAD:master')
+
+    except Exception as e:
+        print("Exception Occurred " + str(e))
